d7a75be4cc0f3ee7049234098e77b2cd1f56399a
Mark Richards <mark@wmrichards.com>
2022-02-16 09:54:10 -0500

    Scripts for Data Sharing Demo
    Scripts for Data Sharing Demo

---
+++ scripts/caching/datasharing/addproduct.sh
@@ -0,0 +1,9 @@
+HOME=$DEMO_HOME
+
+CLASSPATH=$HOME/src/main/resources
+CLASSPATH=$CLASSPATH:$HOME/bin
+CLASSPATH=$CLASSPATH:$HOME/lib/commons-io-1.2.jar
+CLASSPATH=$CLASSPATH:$HOME/lib/rabbitmq-client.jar
+CLASSPATH=$CLASSPATH:$HOME/lib/hazelcast-all-3.10.2.jar
+
+java -cp $CLASSPATH caching.datasharing.AddProduct "$1"
