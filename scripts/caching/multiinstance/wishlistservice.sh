c7f16df71e7bb331bba2c24607427538d83bc363
Mark Richards <mark@wmrichards.com>
2022-02-16 09:53:30 -0500

    Scripts for Multi-Instance Demo
    Scripts for Multi-Instance Demo

---
+++ scripts/caching/multiinstance/wishlistservice.sh
@@ -0,0 +1,11 @@
+HOME=$DEMO_HOME
+
+CLASSPATH=$HOME/src/main/resources
+CLASSPATH=$CLASSPATH:$HOME/bin
+CLASSPATH=$CLASSPATH:$HOME/lib/commons-io-1.2.jar
+CLASSPATH=$CLASSPATH:$HOME/lib/rabbitmq-client.jar
+CLASSPATH=$CLASSPATH:$HOME/lib/hazelcast-all-3.10.2.jar
+CLASSPATH=$CLASSPATH:$HOME/lib/ignite-core-2.8.0.jar
+CLASSPATH=$CLASSPATH:$HOME/lib/cache-api-1.0.0.jar
+
+java -cp $CLASSPATH caching.multiinstance.WishlistService 
